c73745e80ee3e3aca42f438e7015dd08bb259b86
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-12-28 22:18:33 +0400

    Initial setup

---
+++ docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Dragonfly",
+  tagline: "Ultra-fast, scalable in-memory datastore",
+  url: "https://dragonflydb.io",
+  baseUrl: "/docs",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  headTags: [
+    {
+      attributes: {
+        href: "https://fonts.googleapis.com",
+        rel: "preconnect",
+      },
+      tagName: "link",
+    },
+    {
+      attributes: {
+        href: "https://fonts.gstatic.com",
+        rel: "preconnect",
+        crossorigin: "true",
+      },
+      tagName: "link",
+    },
+    {
+      attributes: {
+        href: "https://fonts.googleapis.com/css2?family=Inter&family=Archivo&display=swap",
+        rel: "stylesheet",
+      },
+      tagName: "link",
+    },
+  ],
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/dragonflydb/docs/",
+          routeBasePath: "/",
+        },
+        blog: false,
+        pages: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Docs",
+        logo: {
+          alt: "Dragonfly Logo",
+          src: "img/logo.svg",
+          srcDark: "img/logo-white.svg",
+        },
+        items: [
+          // {
+          //   type: "doc",
+          //   docId: "introduction",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          {
+            href: "https://github.com/dragonflydb/docs/",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          // {
+          //   title: "Docs",
+          //   items: [
+          //     {
+          //       label: "Tutorial",
+          //       to: "/docs/intro",
+          //     },
+          //   ],
+          // },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Attos Technologies Ltd.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
