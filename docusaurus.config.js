c2b07ceeb8577554f71ba60ed260d399b81545d1
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2023-02-03 00:49:52 +0400

    Redesign docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,6 +69,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        disableSwitch: true,
+        defaultMode: "dark",
+      },
       navbar: {
         title: "Docs",
         logo: {
