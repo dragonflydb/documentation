a768bc1e1db3ae64f23b3d1d66dc9af22c79985b
Nick Gottlieb <ngottlieb1@gmail.com>
2024-06-21 10:44:15 -0700

    Update Community.tsx
    Signed-off-by: Nick Gottlieb <ngottlieb1@gmail.com>

--- src/theme/Footer/Community.tsx
+++ src/theme/Footer/Community.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Community = () => {
   return (
     <div className="container">
-      <p>Join the Dragonfly Community to get exclusive contenty</p>
+      <p>Join the Dragonfly Community to get exclusive content.</p>
 
       <a
         type="submit"
